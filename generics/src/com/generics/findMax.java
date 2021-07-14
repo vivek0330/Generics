13782e6f1faead939f60214d0d62460d563d1734
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-15 03:16:59 +0530

    Take 3 veriables of generics types

--- generics/src/com/generics/findMax.java
+++ generics/src/com/generics/findMax.java
@@ -1,20 +1,64 @@
 package com.generics;
 
-public class findMax 
-{
-	public static <E extends Comparable> E max(E... elements) {
-	    E max = elements[0];
-	    for (E element : elements) {
-	        if (element.compareTo(max) > 0) {
-	            max = element;
+import java.util.Scanner;
+
+public class findMax<E extends Comparable> {
+	E x, y, z;
+
+	public findMax(E x, E y, E z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public static <E extends Comparable> E comparesion(E x, E y, E z) {
+	    E max = x;
+	        if (y.compareTo(max) > 0) {
+	            max = y;
 	        }
-	    }
+	        if (z.compareTo(max) > 0) {
+	            max = z;
+	        }
 	    return max;
 	}
+
+	
+
 	public static void main(String[] args) {
-	       System.out.println("Integer Max: " + max(Integer.valueOf(32), Integer.valueOf(56), Integer.valueOf(89), Integer.valueOf(3), Integer.valueOf(456), Integer.valueOf(78), Integer.valueOf(45)));
-	       System.out.println("Double Max: " + max(Double.valueOf(5.6), Double.valueOf(7.8), Double.valueOf(2.9), Double.valueOf(18.6), Double.valueOf(10.25), Double.valueOf(18.6001)));
-	       System.out.println("String Max: " + max("Strawberry", "Mango", "Apple", "Pomegranate", "Guava", "Blackberry", "Cherry", "Orange", "Date"));
-	       
-	   }
+		Scanner sc = new Scanner(System.in);
+		//-----------get float value from user ---------
+		System.out.println("enter the float value :: ");
+		Float firstNumber = sc.nextFloat();
+		Float secondNumber = sc.nextFloat();
+		Float thirdNumber = sc.nextFloat();
+		System.out.println("1st number is :: " + firstNumber);
+		System.out.println("2nd number is :: " + secondNumber);
+		System.out.println("3rd number is :: " + thirdNumber);
+		Float maxNumber = comparesion(firstNumber, secondNumber, thirdNumber);
+		System.out.println("Maximum value is :: " + maxNumber);
+		System.out.println(" ");
+		//------ get integer value form user -------------
+		System.out.println("enter the integer value :: ");
+		Integer first_Number = sc.nextInt();
+		Integer second_Number = sc.nextInt();
+		Integer third_Number = sc.nextInt();
+		System.out.println("1st number is :: " + first_Number);
+		System.out.println("2nd number is :: " + second_Number);
+		System.out.println("3rd number is :: " + third_Number);
+		Integer max_Number = comparesion(first_Number, second_Number, third_Number);
+		System.out.println("Maximum value is :: " + max_Number);
+		System.out.println(" ");
+		//------get string value from user-------------
+		System.out.println("enter the String :: ");
+		String first_String_Number = sc.next();
+		String second_String_Number = sc.next();
+		String third_String_Number = sc.next();
+		System.out.println("1st number is :: " + first_String_Number);
+		System.out.println("2nd number is :: " + second_String_Number);
+		System.out.println("3rd number is :: " + third_String_Number);
+		String max_String_Number = comparesion(first_String_Number, second_String_Number, third_String_Number);
+		System.out.println("Maximum value is :: " + max_String_Number);
+		
+
+	}
 }
