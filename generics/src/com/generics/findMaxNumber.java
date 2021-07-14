3fa77a5e0562084ced308ebab34f2d0c8b0f98e9
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-14 21:43:36 +0530

    given three integers find the maximum

--- generics/src/com/generics/findMaxNumber.java
+++ generics/src/com/generics/findMaxNumber.java
@@ -4,6 +4,27 @@ import java.util.Scanner;
 
 public class findMaxNumber {
 	public static void main(String[] args) {
-		System.out.println("Welcome to the test maximum of 3 variables");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the value :: ");
+		Integer firstNumber = sc.nextInt();
+		Integer secondNumber = sc.nextInt();
+		Integer thirdNumber = sc.nextInt();
+		System.out.println("1st number is :: " + firstNumber);
+		System.out.println("2nd number is :: " + secondNumber);
+		System.out.println("3rd number is :: " + thirdNumber);
+		Integer maxNumber = comparesion(firstNumber, secondNumber, thirdNumber);
+		System.out.println("Maximum value is :: " + maxNumber);
 	}
+
+	private static Integer comparesion(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+		Integer max = firstNumber;
+		if (secondNumber.compareTo(max) > 0) {
+			max = secondNumber;
+		}
+		if (thirdNumber.compareTo(max) > 0) {
+			max = thirdNumber;
+		}
+		return max;
+	}
+
 }
